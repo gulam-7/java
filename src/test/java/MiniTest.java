55f34357a4507bb46112bbae31d34ad5cbe75308
unknown <gulamkhalander@gmail.com>
2025-05-07 18:59:57 +0530

    Initial commit

--- src/test/java/MiniTest.java
+++ src/test/java/MiniTest.java
@@ -1,28 +1,29 @@
-public class MiniTest {
+package com.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MiniTest {
+    
     @Test
     public void simpleTest() {
-        //Setup ChromeDriver path update this to your chromedriver location
-        System.setProperty(key: "webdriver.chrome.driver", value:"./chromedriver.exe");
-    
+        // Setup ChromeDriver path - update this to your chromedriver location
+        System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+        
         // Initialize browser
         WebDriver driver = new ChromeDriver();
-    
+        
         try {
-
             // Open Google
-
             driver.get("https://www.google.com");
-    
+            
             // Print the page title to console
-         System.out.println("Successfully opened:" + driver.getTitle());
-    
-        } 
-    
-        finally {
+            System.out.println("Successfully opened: " + driver.getTitle());
+            
+        } finally {
             // Close browser
             driver.quit();
-        }
-    }
-
+        }
+    }
 }
